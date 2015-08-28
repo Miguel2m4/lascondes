b1d4de15114338f840643dae16b7131c992e453e
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-08-28 18:39:20 -0500

    inicio

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1 user-scalable=no" />
+<meta name="keywords" lang="es" content="Diseño de Sonrisa , Implantes dentales, Rehabilitación oral, Clínica odontológica en Villavicencio, Odontologia en Villavicencio">
+<meta name="robots" content="All">
+<meta name="description" lang="es" content="descripcion de la web">
+<title>Odontología las Condes | Clínica odontológica en Villavicencio</title>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/stylesheet.css" />
+</head>
+<body>
+<header>
+	<div class="Top">
+		<div class="Top-cent">
+			<div class="Top-izq">
+				<img src="images/logo.png">
+			</div>
+			<div class="Top-der">
+				<?php include("menu.php"); ?>
+			</div>
+		</div>
+	</div>
+</header>
+<section>
+
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<ul>
+	<li><a href="">Inicio</a></li>
+	<li><a href="">Diseño de Sonrisa</a></li>
+	<li><a href="">Implantes Dentals</a></li>
+	<li><a href="">Rehabilitación Oral</a></li>
+	<li><a href="">Contáctanos</a></li>
+</ul>

5c9e0fe7999a81f28808b54804fe005ac30ca25b
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-09-02 17:22:55 -0500

    Actualizacion

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 <title>Odontología las Condes | Clínica odontológica en Villavicencio</title>
 <link rel="stylesheet" href="css/normalize.css" />
 <link rel="stylesheet" href="css/stylesheet.css" />
+<link rel="stylesheet" type="text/css" href="css/style-menu.css">
+<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
 </head>
 <body>
 <header>
@@ -20,6 +22,8 @@
 			<div class="Top-der">
 				<?php include("menu.php"); ?>
 			</div>
+			<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+			<script type="text/javascript" src="js/script-menu.js"></script>
 		</div>
 	</div>
 </header>
--- menu.php
+++ menu.php
@@ -1,7 +1,12 @@
-<ul>
+<nav>
+<div class="container">
+<a class="toggleMenu" href="#">Menu</a>
+<ul class="nav">
 	<li><a href="">Inicio</a></li>
 	<li><a href="">Diseño de Sonrisa</a></li>
 	<li><a href="">Implantes Dentales</a></li>
 	<li><a href="">Rehabilitación Oral</a></li>
 	<li><a href="" class="atento">Contáctanos</a></li>
 </ul>
+</div>
+</nav>
